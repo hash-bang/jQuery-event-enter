91083e834d3709dbe1d82d218c4c3a3c522524d4
Matt Carter <m@ttcarter.com>
2012-04-13 13:54:12 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+jQuery Event - Enter
+====================
+Custom jQuery Bind event to capture the enter key being pressed within an input box
+
+	$('#my_input_box').bind('enter', function() { alert('You pressed enter!'); });
+
+or
+
+	$('#my_input_box2').enter(function() { alert('You pressed enter!'); });
---
+++ event-enter.js
@@ -0,0 +1,24 @@
+$(function() {
+/**
+* Custom jQuery Bind event to capture the enter key being pressed within an input box
+* e.g.
+* 	$('#my_input_box').bind('enter', function() { alert('You pressed enter!'); });
+*	$('#my_input_box2').enter(function() { alert('You pressed enter!'); });
+*/
+$.event.special.enter = {
+	setup: function(data, namespaces) {
+		$(this).bind('keydown', jQuery.event.special.enter.handler);
+	},
+
+	teardown: function(namespaces) {
+		$(this).unbind('keydown', jQuery.event.special.enter.handler);
+	},
+
+	handler: function(event) {
+		if (event.which == 13) {
+			event.type = "enter";
+			jQuery.event.handle.apply(this, arguments)
+		}
+	}
+}
+});
