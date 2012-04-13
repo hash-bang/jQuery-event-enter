196746169bf5ba1fee014bb634bab265bf4d83b8
Matt Carter <m@ttcarter.com>
2012-04-13 13:56:05 +1000

    Added about speil to top of main JS file

--- event-enter.js
+++ event-enter.js
@@ -1,5 +1,9 @@
 $(function() {
 /**
+* @author Matt Carter <m@ttcarter.com>
+* @version 1.0.0
+* @url https://github.com/hash-bang/jQuery-event-enter
+*
 * Custom jQuery Bind event to capture the enter key being pressed within an input box
 * e.g.
 * 	$('#my_input_box').bind('enter', function() { alert('You pressed enter!'); });
